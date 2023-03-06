f02cbda841971377fc651e9715bec9a2df2311b7
Volodymyr Osadchuk <vovavov04@gmail.com>
2023-03-06 08:34:19 +0200

    rem comments

--- volodymyr-app.js
+++ volodymyr-app.js
@@ -2,11 +2,9 @@
 
 const express = require('express');
 
-// константы
 const port = 80;
 const host = '0.0.0.0';
 
-// приложение
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello from Volodymyr Osadchuk');
